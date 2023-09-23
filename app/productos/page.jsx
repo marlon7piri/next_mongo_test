7bb365ff369bb45b4579dbbb057d846b1e45e3b9
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2023-09-23 00:30:41 -0500

    ssjkjjd

--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -7,7 +7,7 @@ import CardProductos from "@/components/CardProductos";
 
 
  const useFetch = async () => {
-  const res = await fetch("http://localhost:3000/productos");
+  const res = await fetch("http://localhost:4000/productos");
  
   const data = await res.json();
 
