37397f979509d8eeb5dccb77bcde799f8a72c3e7
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2023-09-23 00:07:38 -0500

    delted prisma

--- app/api/products/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/libs/prisma.js";
-
-export async function GET() {
-  const allproducts = await prisma.products.findMany();
-
-  return NextResponse.json(allproducts);
-}
